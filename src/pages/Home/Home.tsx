f8681c9e599c942104bcc664a31a127db3d631c7
eduardozuppodev <zuppo.edu@gmail.com>
2021-11-27 16:38:53 -0300

    feat: home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header, Card } from '../../components';
+import api from '../../services/api';
 import { Container, Repos, Credits } from './styles';
 
 const Home: React.FC = () => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('/eduardozuppodev/repos').then(({ data }) => {
+      setRepos(data);
+    });
+  }, []);
+
   return (
     <Container>
       <Header title="MyRepos" />
       <Repos>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {repos.map((repo: ReposType, id: number) => (
+          <Card key={id} title={repo.name} description={repo.description} />
+        ))}
         <Credits>Desenvolvido por Eduardo Zuppo</Credits>
       </Repos>
     </Container>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { colors } from '../../themes';
 
 export const Container = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100%;
   background: #f4f4f4;
 `;
