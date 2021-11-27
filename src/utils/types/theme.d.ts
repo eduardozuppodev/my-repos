18aa3123132df0c11046d5d0bc9827afd3265461
eduardozuppodev <zuppo.edu@gmail.com>
2021-11-27 10:18:21 -0300

    feat: types

---
+++ src/utils/types/theme.d.ts
@@ -0,0 +1,17 @@
+declare type RadiusType = {
+  smallRadius: string;
+  mediumRadius: string;
+  largeRadius: string;
+  modalRadius: string;
+  buttonRadius: string;
+};
+
+declare type Spacings = {
+  sceneSpacing: string;
+  smallSpacing: string;
+  mediumSpacing: string;
+  largeSpacing: string;
+  sectionSpacing: string;
+  screenWidth: number;
+  screenHeight: number;
+};
