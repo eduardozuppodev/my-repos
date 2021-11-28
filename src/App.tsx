6e6d6fcc3bec4dc937c284e4c82f043349809c27
eduardozuppodev <zuppo.edu@gmail.com>
2021-11-28 18:28:14 -0300

    fix: import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './routes';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './stores';
 
 const App = () => (
   <Provider store={store}>
