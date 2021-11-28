138fe75b01edaf51d523465a490ab0d8e5feeffc
eduardozuppodev <zuppo.edu@gmail.com>
2021-11-28 12:36:34 -0300

    feat: add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Routes from './routes';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const App = () => <Routes />;
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+);
 
 export default App;
